b715b5f57c72aec25a97f227c74186b95796740b
Nath Arceo <natharceo7@gmail.com>
2021-11-27 19:22:00 -0800

    Week4 Workshop Assignment

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '201221162087836',
+        clientSecret: '703f4c864c96f7d479e9a17039daa4f7'
+    }
 }
